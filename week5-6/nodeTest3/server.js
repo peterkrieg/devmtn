e625fc51be48105956f0fc2c001935e1f4f8b9e2
Peter Krieg <pkrieg2@gmail.com>
2015-11-16 11:40:01 -0700

    morning edits to nodeTest3

--- week5-6/nodeTest3/server.js
+++ week5-6/nodeTest3/server.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var usersCtrl = require('./controllers/usersCtrl');
+var moviesCtrl = require('./controllers/moviesCtrl');
+var cors = require('cors');
+var session = require('express-session');
+
+
+var logger = function(req, res, next) {
+  console.log('\n\n------------------------------\n------------------------------\n\n');
+  console.log('\n___HEADERS___\n', req.headers);
+  console.log('\n___BODY___\n', req.body);
+  console.log('\n___SESSION___\n', req.session);
+  next();
+};
 
 var app = express();
 
+// app.use(express.static('public'));
+
+// app.use(cors());
+// app.options('*', cors());
+
+app.use(session({
+  secret: 'gweriwrb-erfawrg45-oasWsd',
+  saveUninitialized: false,
+  resave: false
+}));
+
+
 app.use(bodyParser.json());
 
-var usersCtrl = require('./controllers/usersCtrl');
-var moviesCtrl = require('./controllers/moviesCtrl');
+app.use(logger);
+
+
+
+
+app.use(function(req, res, next){
+	console.log('path', req.path);
+	console.log('body', req.body);
+	next();
+});
 
 
 app.get('/users',  usersCtrl.index);
@@ -14,6 +47,15 @@ app.get('/users/:id', usersCtrl.show);
 app.post('/users', usersCtrl.build);
 app.put('/users/:id', usersCtrl.update);
 app.delete('/users/:id', usersCtrl.destroy);
+
+app.post('/cart', function(req, res, next){
+	if(!req.session.cart){
+		req.session.cart = [];
+	}
+	req.session.cart.push(req.body);
+	res.status(200).json(req.session.cart)
+
+})
 
 // app.get('/movies',  moviesCtrl.index);
 // app.get('/movies/:id', moviesCtrl.show);
